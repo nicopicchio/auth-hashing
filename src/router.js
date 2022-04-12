cb92b4c9cf2a21f5ab36fb57bf618efbd5346919
Nico Picchio <nicolapicchio@gmail.com>
2022-04-12 15:59:35 +0100

    exercise completed

--- src/router.js
+++ src/router.js
@@ -3,15 +3,38 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
 const router = express.Router();
+const superSecretKey = 'jsdnvgjnslgvnhjjljrn'
 
 router.post('/register', async (req, res) => {
-    
+	try {
+		const registeredUser = await prisma.user.create({
+			data: {
+				username: req.body.username,
+				password: await bcrypt.hash(req.body.password, 10),
+			},
+		});
+		res.status(200).json({ registeredUser });
+	} catch (e) {
+		console.error(e);
+		res.status(500).json('Something went wrong!');
+	}
 });
 
 router.post('/login', async (req, res) => {
-    
+	const typedPassword = req.body.password;
+	const matchingUser = await prisma.user.findUnique({
+		where: {
+			username: req.body.username,
+		},
+	});
+	const matchingPassword = await bcrypt.compare(typedPassword, matchingUser.password);
+	if (matchingUser && matchingPassword) {
+		const token = jwt.sign({ username: matchingUser.username }, superSecretKey);
+		res.status(200).json({ token });
+		return;
+	}
+	res.status(401).json('Access denied!');
 });
 
 module.exports = router;
